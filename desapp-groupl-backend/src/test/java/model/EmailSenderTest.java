51940e82dc10ff7bd880fb4ead52e70eb4938b5b
Ruben Novelli <rubendario.novel@gmail.com>
2019-04-18 09:15:58 -0300

    Test de email

--- desapp-groupl-backend/src/test/java/model/EmailSenderTest.java
+++ desapp-groupl-backend/src/test/java/model/EmailSenderTest.java
@@ -1,8 +1,11 @@
 package model;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+
+import javax.mail.SendFailedException;
 
 import org.junit.Test;
 
@@ -10,17 +13,26 @@ import ar.edu.unq.grupol.app.model.EmailSender;
 
 public class EmailSenderTest {
 
-    @Test
-    public void testEnvioDeEmail() {
-    	EmailSender emailMock = mock(EmailSender.class);
-    	emailMock.send(any(),any());
-    	verify(emailMock, times(1)).send(any(),any());
-    }
-    
-    @Test
-    public void testParaVerificarLaInstancia() {
-    	EmailSender emailSenderInstance = EmailSender.getInstance();
-    	assertNotNull(emailSenderInstance);
-    }
-    
+	@Test
+	public void testEnvioDeEmail() {
+		EmailSender emailMock = mock(EmailSender.class);
+		emailMock.send(any(), any());
+		verify(emailMock, times(1)).send(any(), any());
+	}
+
+	@Test
+	public void testParaVerificarLaInstancia() {
+		EmailSender emailSenderInstance = EmailSender.getInstance();
+		assertNotNull(emailSenderInstance);
+	}
+
+	@Test
+	public void testParaVerificarDireccionEnvioInvalido() {
+		try {
+	    	EmailSender emailSenderInstance = EmailSender.getInstance();
+	    	emailSenderInstance.send("", "");
+		} catch (Exception e) {
+			System.out.println("gil");
+		}
+	}
 }

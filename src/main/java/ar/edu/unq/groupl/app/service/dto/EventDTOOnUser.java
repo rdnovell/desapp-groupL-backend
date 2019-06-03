9ab60d3adb8333cee625e58e875561f0c80ed4f9
Ruben Novelli <rubendario.novel@gmail.com>
2019-06-03 15:30:06 -0300

    Se agrega lista de invitados

--- src/main/java/ar/edu/unq/groupl/app/service/dto/EventDTOOnUser.java
+++ src/main/java/ar/edu/unq/groupl/app/service/dto/EventDTOOnUser.java
@@ -2,6 +2,8 @@ package ar.edu.unq.groupl.app.service.dto;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import ar.edu.unq.groupl.app.model.Event;
 import ar.edu.unq.groupl.app.model.Item;
 import lombok.Getter;
@@ -12,12 +14,14 @@ public abstract class EventDTOOnUser {
 	private Integer id;
 	private String title;
 	private List<Item> items;
+	private List<String> guests;
 	private LocalDate date;
 	
 	public EventDTOOnUser(Event event) {
 		this.id = event.getId();
 		this.title = event.getTitle();
 		this.items = event.getItems();
+		this.guests = event.getGuests().stream().map(e -> e.getEmail()).collect(Collectors.toList());
 		this.date = event.getDate();
 	}
 

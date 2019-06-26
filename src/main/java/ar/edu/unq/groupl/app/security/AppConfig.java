927ba4bd43cad5ce648c435c85fbac25a58b7fe8
Ruben Novelli <rubendario.novel@gmail.com>
2019-06-26 12:11:03 -0300

    Security

---
+++ src/main/java/ar/edu/unq/groupl/app/security/AppConfig.java
@@ -0,0 +1,37 @@
+package ar.edu.unq.groupl.app.security;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.http.HttpMethod.GET;
+
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@Order(99)
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+    @Value(value = "${auth0.apiAudience}")
+    private String apiAudience;
+    @Value(value = "${auth0.issuer}")
+    private String issuer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .authorizeRequests()
+                .antMatchers(GET, "/").permitAll()
+                .antMatchers(GET, "/api/loan/**").authenticated();
+
+        http.headers().frameOptions().sameOrigin();
+    }
+
+}
--- src/main/java/ar/edu/unq/groupl/app/service/aspect/ServiceLoggerAspect.java
+++ src/main/java/ar/edu/unq/groupl/app/service/aspect/ServiceLoggerAspect.java
@@ -1,14 +1,15 @@
 package ar.edu.unq.groupl.app.service.aspect;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import java.sql.Timestamp;
-import java.util.Arrays;
+
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Aspect
 @Component
@@ -17,12 +18,12 @@ public class ServiceLoggerAspect {
 	@SuppressWarnings("unchecked")
 	@Around(value = "@annotation(ar.edu.unq.groupl.app.service.annotation.Log)")
 	public <T> T logAspect(ProceedingJoinPoint joinPoint) throws Throwable {
-		Logger logger = Logger.getLogger(joinPoint.getTarget().getClass().getSimpleName());
+		Object token = SecurityContextHolder.getContext().getAuthentication().getCredentials();
+        DecodedJWT decode = JWT.decode(token.toString());
+        Logger logger = Logger.getLogger(joinPoint.getTarget().getClass().getSimpleName());
 		try {
 			T toReturn = (T) joinPoint.proceed();
-			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-			String parameters = Arrays.stream(joinPoint.getArgs()).map(a -> "Type: " + a.getClass().getSimpleName() + " Value: " + a ).collect(Collectors.joining());
-			logger.info("Method " + joinPoint.getSignature().getName() + " executed correctly with parameters [" + parameters + "].");
+			logger.info("Method " + joinPoint.getSignature().getName() + " executed correctly by user " + decode.getClaim("email").asString() + ".");
 			return toReturn;
 		} catch (Throwable throwable) {
 			 Function<String, String> exceptionMessage = message -> message == null ? "." : " - Exception message: " + message;

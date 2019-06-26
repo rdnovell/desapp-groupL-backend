fd5d23ee83fcd268ee979de46a23bc5d852545fe
Ruben Novelli <rubendario.novel@gmail.com>
2019-06-26 15:12:54 -0300

    prueba

--- src/main/java/ar/edu/unq/groupl/app/service/aspect/ServiceLoggerAspect.java
+++ src/main/java/ar/edu/unq/groupl/app/service/aspect/ServiceLoggerAspect.java
@@ -14,22 +14,30 @@ import java.util.function.Function;
 @Aspect
 @Component
 public class ServiceLoggerAspect {
-	
+
 	@SuppressWarnings("unchecked")
 	@Around(value = "@annotation(ar.edu.unq.groupl.app.service.annotation.Log)")
 	public <T> T logAspect(ProceedingJoinPoint joinPoint) throws Throwable {
 		Object token = SecurityContextHolder.getContext().getAuthentication().getCredentials();
-        DecodedJWT decode = JWT.decode(token.toString());
-        Logger logger = Logger.getLogger(joinPoint.getTarget().getClass().getSimpleName());
+
+		Logger logger = Logger.getLogger(joinPoint.getTarget().getClass().getSimpleName());
+		String user = "";
+		try {
+			DecodedJWT decode = JWT.decode(token.toString());
+			user = decode.getClaim("email").asString();
+		}catch (Exception e){
+			user = "inv";
+		}
 		try {
 			T toReturn = (T) joinPoint.proceed();
-			logger.info("Method " + joinPoint.getSignature().getName() + " executed correctly by user " + decode.getClaim("email").asString() + ".");
+			logger.info("Method " + joinPoint.getSignature().getName() + " executed correctly by user " + user + ".");
 			return toReturn;
 		} catch (Throwable throwable) {
-			 Function<String, String> exceptionMessage = message -> message == null ? "." : " - Exception message: " + message;
-			 logger.error("Error on method: " + joinPoint.getSignature().getName() + exceptionMessage.apply(throwable.getMessage()));
-			 throw throwable;
+			Function<String, String> exceptionMessage = message -> message == null ? "." : " - Exception message: " + message;
+			logger.error("Error on method: " + joinPoint.getSignature().getName() + exceptionMessage.apply(throwable.getMessage()));
+			throw throwable;
 		}
 	}
-	
+
 }
+
